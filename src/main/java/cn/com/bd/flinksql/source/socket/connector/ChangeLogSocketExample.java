9912de08c98188917020335706a97524edaafc36
刘天能 <liutianneng@bluemoon.com.cn>
2021-05-14 15:17:47 +0800

    修改操作描述

--- src/main/java/cn/com/bd/flinksql/source/socket/connector/ChangeLogSocketExample.java
+++ src/main/java/cn/com/bd/flinksql/source/socket/connector/ChangeLogSocketExample.java
@@ -7,7 +7,8 @@ import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
  *  操作指引
  *  1. 进入192.168.235.19的网关机
  *  2. nc -lk 9999
- *  3. 输入 INSERT|Alice|12 类型数据回车
+ *  3. 输入 INSERT|Alice|12 此样例数据
+ *      输入数据为三列数据，分隔符为'|'，第一列可以是INSERT/DELETE，第二列为string，第三列为int
  */
 public class ChangeLogSocketExample {
     public static void main(String[] args) {
