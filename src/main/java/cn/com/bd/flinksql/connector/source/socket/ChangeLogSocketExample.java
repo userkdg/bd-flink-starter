7c32db95d4fdf50600bd1071c6981aa661862c45
刘天能 <liutianneng@bluemoon.com.cn>
2021-05-21 11:10:11 +0800

    修改自定义source写入HBase

--- src/main/java/cn/com/bd/flinksql/connector/source/socket/ChangeLogSocketExample.java
+++ src/main/java/cn/com/bd/flinksql/connector/source/socket/ChangeLogSocketExample.java
@@ -7,7 +7,7 @@ import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
  *  操作指引
  *  1. 进入192.168.235.19的网关机
  *  2. nc -lk 9999
- *  3. 输入 INSERT|Alice|12 此样例数据
+ *  3. 输入 DELETE|Alice|12 此样例数据
  *      输入数据为三列数据，分隔符为'|'，第一列可以是INSERT/DELETE，第二列为string，第三列为int
  */
 public class ChangeLogSocketExample {
@@ -30,6 +30,16 @@ public class ChangeLogSocketExample {
                 ")");
 
 //        tableEnv.executeSql("select * from userScore").print();
-        tableEnv.executeSql("select name, avg(score) as avg_score from userScore group by name").print();
+        tableEnv.executeSql("create table delete_test(" +
+                " rowkey string," +
+                " info row<name string, score int>," +
+                " primary key(rowkey) not enforced" +
+                ") with (" +
+                " 'connector' = 'hbase-1.4',\n" +
+                " 'table-name' = 'delete_test',\n" +
+                " 'zookeeper.quorum' = '192.168.235.51:2181,192.168.235.52:2181,192.168.235.53:2181')");
+
+
+        tableEnv.executeSql("insert into delete_test select name as rowkey, row(name, score) from userScore");
     }
 }
